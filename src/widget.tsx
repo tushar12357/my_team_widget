97a055f4d68b82051cba5f03cb538e91835dd559
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2025-10-05 23:37:48 +0530

    vercel

--- src/widget.tsx
+++ src/widget.tsx
@@ -16,7 +16,7 @@ class ReactWidget extends HTMLElement {
     const container = document.createElement("div");
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "https://ravanai-iota.vercel.app/react-widget-uv.css";
+    link.href = "https://my-team-widget.vercel.app/style.css";
 
     // Append the stylesheet and container to the Shadow DOM
     this.shadowRoot?.appendChild(link);
